c3114cb8d32a0b689a7469346541cd7a9aa53bde
githubadminadmin <forgithubproject@gmail.com>
2019-03-15 17:29:54 +0300

    15.03.2019

--- plitka/settings.py
+++ plitka/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
             'USER': 'adminadmin',
             'PASSWORD': 'fuckthesistem1',
             'HOST': 'localhost',
-            'PORT': '5432',
+            'PORT': '',
     }
 }
 
